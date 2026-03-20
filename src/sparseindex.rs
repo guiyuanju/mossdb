0269757325dc1d0555fe35f1dc924a4053e0bc23
guiyuanju <guiyuanjune@outlook.com>
2026-03-20 17:14:17 +0800

    fix index

--- src/sparseindex.rs
+++ src/sparseindex.rs
@@ -1,4 +1,4 @@
-use crate::types::Offset;
+use crate::{reader::CachedReader, types::Offset};
 
 #[derive(Debug)]
 pub struct SparseIndex {
@@ -10,17 +10,19 @@ impl SparseIndex {
         Self { index }
     }
 
-    pub fn get_closed_indice(&self, key: &str) -> Option<(Offset, Offset)> {
+    pub fn populate_from_log(filename: &str, reader: &CachedReader) {}
+
+    pub fn get_containing_block_offset(&self, key: &str) -> Option<u64> {
         let res = self
             .index
             .binary_search_by_key(&key.to_string(), |(k, _)| k.to_string()); // TODO: a lot of to_string
         match res {
-            Ok(offset) => Some((offset as u64, offset as u64)),
+            Ok(offset) => Some(offset as u64),
             Err(offset) => {
                 if offset == 0 {
                     return None;
                 }
-                return Some((offset as u64 - 1, offset as u64));
+                return Some(offset as u64 - 1);
             }
         }
     }
