d7b458b59c580ed88a7565cdc781feb53e249067
guiyuanju <guiyuanjune@outlook.com>
2026-03-20 17:14:25 +0800

    clean sstable

--- src/sstable.rs
+++ src/sstable.rs
@@ -1,5 +1,5 @@
-use crate::reader::Reader;
-use crate::sparseindex::SparseIndex;
+use crate::reader::CachedReader;
+use crate::sparseindex::{self, SparseIndex};
 use crate::types::{Key, Offset, Value};
 use anyhow::Result;
 use anyhow::anyhow;
@@ -7,47 +7,29 @@ use anyhow::anyhow;
 #[derive(Debug)]
 pub struct SSTable {
     sparse_index: SparseIndex,
-    reader: Reader,
+    reader: CachedReader,
     pub filename: String,
 }
 
 impl SSTable {
-    pub fn new(filename: String) -> Self {
-        Self {
-            sparse_index: SparseIndex::new(vec![]),
-            reader: Reader::new(),
+    pub fn new(filename: String) -> Result<Self> {
+        let mut reader = CachedReader::new(filename.clone());
+        let index = reader.read_sparse_index()?;
+        let sparseindex = SparseIndex::new(index);
+        Ok(Self {
+            sparse_index: sparseindex,
+            reader: reader,
             filename,
-        }
+        })
     }
 
     pub fn get(&mut self, key: &str) -> Result<String> {
-        let offset_range = self
+        let block_offset = self
             .sparse_index
-            .get_closed_indice(key)
+            .get_containing_block_offset(key)
             .ok_or(anyhow!("not found in current sstable"))?;
 
-        let mut cur_offset = offset_range.0;
-        while cur_offset < offset_range.1 {
-            let meta = self.reader.readMeta(&self.filename, cur_offset)?;
-
-            let cur_key = self
-                .reader
-                .read(&self.filename, meta.key_offset, meta.key_len)?;
-
-            // found the key, read and return the val
-            if cur_key == key.as_bytes() {
-                let cur_val = self
-                    .reader
-                    .read(&self.filename, meta.val_offset, meta.val_len)?;
-                return Ok(String::from_utf8_lossy(&cur_val).to_string());
-            }
-
-            // not found, continue
-            cur_offset = meta.val_offset + meta.val_len;
-        }
-
-        // not found
-        Err(anyhow!("not found in current sstable"))
+        self.reader.read_key(block_offset, key)
     }
 
     pub fn len(&self) -> usize {
